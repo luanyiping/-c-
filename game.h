57f7ece0eefde0d7f0750298bf57ff7e35e6fd5e
luanyiping <75729730+luanyiping@users.noreply.github.com>
2021-01-24 20:46:02 +0800

    Delete game.h

--- game.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#define ROW 5
-#define COL 5
-void InitBoard(char board[ROW][COL], int row, int col);
-void DisplayBoard(char board[ROW][COL], int row, int col);
-void PlayerMove(char board[ROW][COL],int row,int col);
-void ComputerMove(char board[ROW][COL], int row, int col);
-char CheckWin(char board[ROW][COL], int row, int col);
