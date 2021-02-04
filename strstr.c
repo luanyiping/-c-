1da2f14e94ff415b0caa8f88635afb79c534e0ae
luanyiping <3349244951@qq.com>
2021-02-04 20:21:16 +0800

    strstr模拟

---
+++ strstr.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<assert.h>
+char* my_strstr(const char* s1,const char* s2)
+{
+	assert(s1 && s2);
+	const char* cp = s1;
+	while (*cp)
+	{
+		const char* p1 = cp;
+		const char* p2 = s2;
+		while ((*p1) && (*p2) && (*p1 == *p2))
+		{
+			p1++;
+			p2++;
+		}
+		if (*p2 == '\0')
+			return (char*)cp;
+		cp++;
+	}
+	return NULL;
+}
+int main()
+{
+	char arr1[] = "abbbcde";
+	char arr2[] = "bbc";
+	char* ret = my_strstr(arr1, arr2);
+	if (ret != NULL)
+	{
+		printf("%s\n", ret);
+	}
+}
