8013400c842e35a4bd2c6a78e2c2832004f6e89f
luanyiping <3349244951@qq.com>
2021-02-05 17:13:58 +0800

    strcat

---
+++ strcat.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<assert.h>
+char* my_strcat(char* dest, const char* src)
+{
+	assert(dest && src);
+	char* ret = dest;
+	while (*dest)
+	{
+		dest++;
+	}
+	while (*dest++ = *src++);
+	return ret;
+}
+int main()
+{
+	char arr1[20] = "hello ";
+	char arr2[] = "world";
+	printf("%s\n", my_strcat(arr1, arr2));
+}
