2476d3b7355daf664d6cb6b54bb5dbd6ba26e289
luanyiping <3349244951@qq.com>
2021-02-06 21:15:27 +0800

    字符内存操作函数

---
+++ 字符内存操作函数/strcpy.c
@@ -0,0 +1,17 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<assert.h>
+#include<string.h>
+char* my_strcpy(char* dest, const char* src)
+{
+	assert(dest && src);
+	char* ret = dest;
+	while (*dest++ = *src++);
+	return ret;
+}
+int main()
+{
+	char arr1[20] = "abcdef";
+	char arr2[] = "lkj";
+	printf("%s\n", strcpy(arr1, arr2));
+}
