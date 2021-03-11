55f7d9d55c9e40c12fa93d0a7ff835d2d02d90fc
luanyiping <75729730+luanyiping@users.noreply.github.com>
2021-03-11 15:40:12 +0800

    Update Contect.h

--- 通讯录/Contect.h
+++ 通讯录/Contect.h
@@ -1,30 +1,58 @@
 #pragma once
-#define NAME_MAX 10
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include <malloc.h>
+ 
+#define MAX 1000
+#define NAME_MAX 20
 #define SEX_MAX 5
-#define TEL_MAX 20
-#define ADDR_MAX 20
-#define PEOPLE_MAX 1000
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-struct PeopleInfor
+#define ADDR_MAX 30
+#define TELE_MAX 11
+#define DEFAULT_SZ 1
+#define DEFAULT_INC 1
+enum
+{
+	EXIT,
+	ADD,
+	DEL,
+	MODIFY,
+	SEARCH,
+	SHOW,
+	SORT,
+	EMPTY,
+	SAVE,
+};
+typedef struct PeoInfo
 {
 	char name[NAME_MAX];
-	char sex[SEX_MAX];
 	int age;
-	char tel[TEL_MAX];
+	char sex[SEX_MAX];
 	char addr[ADDR_MAX];
-};
-struct Contect
+	char tele[TELE_MAX];
+}PeoInfo;
+ 
+typedef struct Contact 
 {
-	struct PeopleInfor data[PEOPLE_MAX];
-	int size;
-};
-void ContestInit(struct Contect* pc);
-void ContectAdd(struct Contect* pc);
-void ContectShow(struct Contect* pc);
-void ContectDelete(struct Contect* pc);
-void ContectSearch(struct Contect* pc);
-void ContectModity(struct Contect* pc);
-void ContectEmpty(struct Contect* pc);
-void ContectSort(struct Contect* pc);
+	//PeoInfo  data[MAX];
+	PeoInfo  *data;  //动态内存开辟
+	int sz;
+	int capacity;  //用来维护动态内存开辟的容量
+}Contact,*pContact;
+ 
+void InitContact(pContact p);
+void AddContact(pContact p);
+void DelContact(pContact p);
+void ModifyContact(pContact p);
+void SearchContact(pContact p);
+void EmptyContact(pContact p);
+void DestroyContact(pContact p);
+//void SortInfo(pContact p);
+void Sort_Name(pContact p);
+ 
+void LoadContact(pContact p);
+void SaveContact(pContact p);
+ 
+void ShowContact(const pContact p);
+static int Find(pContact p);
